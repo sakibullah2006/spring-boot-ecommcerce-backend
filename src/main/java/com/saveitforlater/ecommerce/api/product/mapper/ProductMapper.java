81db96d75f59d385e5e1e04f10aa53f8ceca577b
sakibullah2006 <sakibullah.dev@gmail.com>
2025-11-25 01:11:12 +0600

    - ProductMapper added - ProductRepository added

---
+++ src/main/java/com/saveitforlater/ecommerce/api/product/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.saveitforlater.ecommerce.api.product.mapper;
+
+import com.saveitforlater.ecommerce.api.product.dto.ProductResponse;
+import com.saveitforlater.ecommerce.persistence.entity.category.Category;
+import com.saveitforlater.ecommerce.persistence.entity.product.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper // componentModel="spring" is set globally in your pom.xml
+public interface ProductMapper {
+
+    @Mapping(source = "publicId", target = "id")
+    ProductResponse toProductResponse(Product product);
+
+    @Mapping(source = "publicId", target = "id")
+    ProductResponse.CategorySummary toCategorySummary(Category category);
+}
--- src/main/java/com/saveitforlater/ecommerce/persistence/repository/product/ProductRepository.java
+++ src/main/java/com/saveitforlater/ecommerce/persistence/repository/product/ProductRepository.java
@@ -1,4 +1,5 @@
 package com.saveitforlater.ecommerce.persistence.repository.product;
+
 import com.saveitforlater.ecommerce.persistence.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
